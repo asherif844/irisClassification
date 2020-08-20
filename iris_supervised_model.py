5b531b11bb508a54a0149e466aa61d92f4830f2e
Ahmed Sherif <Ahmed.sherif@att.net>
2020-08-20 07:38:48 -0400

    data

---
+++ iris_supervised_model.py
@@ -0,0 +1,104 @@
+# Check the versions of libraries
+ # Python version
+
+import sys
+import scipy
+import numpy as np
+# import matplotlib.pyplot as plt
+import matplotlib
+import pandas as pd
+import sklearn
+from sklearn import model_selection
+
+print('======================================')
+print('Python:      {}'.format(sys.version))
+print('scipy:       {}'.format(scipy.__version__))
+print('numpy:       {}'.format(np.__version__))
+print('matplotlib:  {}'.format(matplotlib.__version__))
+print('pandas:      {}'.format(pd.__version__))
+print('sklearn:     {}'.format(sklearn.__version__))
+print('======================================')
+
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+column_headers = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+df = pd.read_csv(url, names=column_headers)
+
+# df.to_csv('rawdata.csv')
+# print(df.shape)
+# print(df.head())
+# print(df.describe())
+# print(df.groupby('class').size())
+# df.plot(kind = 'box', subplots=True, layout = (2,2), sharex=False, sharey=False)
+# plt.show()
+data = df.values
+X = data[:, 0:4]
+y = data[:,4]
+validation_size = 0.20
+seed = 12345
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(
+    X, y, test_size=validation_size, random_state=seed)
+
+# We will use 10-fold cross validation to estimate accuracy.
+print('X_train: {}'.format(X_train.shape))
+print('X_validation: {}'.format(X_validation.shape))
+print('Y_train: {}'.format(Y_train.shape))
+print('Y_validation: {}'.format(Y_validation.shape))
+
+#import models
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+scoring = 'accuracy'
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+results = []
+names = []
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(
+		model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
+
+
+# Make predictions on validation dataset
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+lda = LinearDiscriminantAnalysis()
+lda.fit(X_train, Y_train)
+predictions = lda.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+
+filename = 'finalized_model.sav'
+
+# export model
+import pickle
+pickle.dump(lda, open(filename, 'wb'))
+
+# load model
+
+loaded_model = pickle.load(open(filename, 'rb'))
+result = loaded_model.score(X_train, Y_train)
+print(result)
+
+from sklearn.externals import joblib
+joblib.dump(lda, 'finalized_model.pkl')
